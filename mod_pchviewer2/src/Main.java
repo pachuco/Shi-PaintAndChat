0c1d7f0bc013bc105894f3526c587fe2315ce299
pachuco <ifail@life.net>
2019-01-06 18:04:02 +0200

    Minor tinkering.

--- mod_pchviewer2/src/Main.java
+++ mod_pchviewer2/src/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        jaba.applet.Applet.d_setupDesktop("pchviewer.ini", "pchviewer_v1");
+        jaba.applet.Applet.d_setupDesktop("pchviewer.ini", "pchviewer_v2");
         Applet client = new pch2.PCHViewer();
         client.init();
         client.start();
